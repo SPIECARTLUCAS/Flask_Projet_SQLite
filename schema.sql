e3d0ae7570a876887c514ff7f13e2537a652ac3f
SPIECARTLUCAS <lucas.spiecart@gmail.com>
2025-01-23 10:02:10 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,4 +13,36 @@ CREATE TABLE IF NOT EXISTS Livre (
     genre TEXT,
     disponible INTEGER DEFAULT 1
 );
+-- Table pour les livres
+CREATE TABLE Livres (
+    id_livre INT AUTO_INCREMENT PRIMARY KEY,
+    titre VARCHAR(255) NOT NULL,
+    auteur VARCHAR(255) NOT NULL,
+    annee_publication YEAR,
+    genre VARCHAR(100),
+    isbn VARCHAR(20) UNIQUE,
+    nbre_exemplaires VARCHAR(2) NOT NULL
+);
+
+-- Table pour les utilisateurs
+CREATE TABLE Utilisateurs (
+    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    date_inscription DATE DEFAULT CURRENT_DATE
+);
+
+
+-- Table pour les emprunts
+CREATE TABLE Emprunts (
+    id_emprunt INT AUTO_INCREMENT PRIMARY KEY,
+    id_utilisateur INT NOT NULL,
+    id_livre INT NOT NULL,
+    date_emprunt DATE DEFAULT CURRENT_DATE,
+    date_retour_prevue DATE,
+    date_retour_effective DATE,
+    FOREIGN KEY (id_utilisateur) REFERENCES Utilisateurs(id_utilisateur) ON DELETE CASCADE,
+    FOREIGN KEY (id_livre) REFERENCES Livres(id_livre) ON DELETE CASCADE
+);
 
